76488a4dc6c0c1816d53d1ba5eac048804c745c7
Ard van Breemen <ard@kwaak.net>
2013-06-12 19:49:24 +0200

    nagios nrpe check

---
+++ backend/management/www/bin/check_tunnels.lua
@@ -0,0 +1,39 @@
+#!/usr/bin/lua
+
+-- ElCheapo mysql interface
+require "luasql.mysql"
+local file="/etc/oeps/config"
+local co={}
+local code
+if _G._VERSION == "Lua 5.1"
+then
+	code=assert(loadfile(file,"t"))
+	if code then setfenv(code,co) end
+else
+	code=assert(loadfile(file,"t",co))
+end
+code()
+
+local exitcode=0
+local status="OK"
+local message=""
+local dbi=luasql.mysql()
+local dbh=dbi:connect(co.dbname,co.dbuser,co.dbpassword)
+local res=dbh:execute([[
+select name,state from ap where state != "UP" and adminstate = "PRODUCTION" and TIMESTAMPDIFF(DAY,lastcontact,NOW())<7
+]])
+row=res:fetch({},"a")
+while row
+do
+	exitcode=1
+	message=message .. " " .. row.name .. ":" ..row.state
+	status="CRITICAL:"
+	row=res:fetch({},"a")
+end
+
+
+res:close()
+dbh:close()
+dbi:close()
+print(status .. message)
+os.exit(exitcode)
