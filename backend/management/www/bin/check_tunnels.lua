048570cc6198b125388beec38776658099932bfe
Ard van Breemen <ard@telegraafnet.nl>
2014-04-16 12:57:09 +0200

    simple tunnel check for icinga

--- backend/management/www/bin/check_tunnels.lua
+++ backend/management/www/bin/check_tunnels.lua
@@ -1,30 +1,22 @@
 #!/usr/bin/lua
--- ElCheapo mysql interface
-require "luasql.mysql"
+require "oeps.db"
+local dbh=assert(oeps.dbh())
+local sth=assert(dbh:prepare[[
+	select name,state from ap where state != "UP" and adminstate = "PRODUCTION" and TIMESTAMPDIFF(DAY,lastcontact,NOW())<7
+]])
 
-local c=require("oeps.config")
-local co=c.getconfig()
+sth:execute()
 
 local exitcode=0
 local status="OK"
 local message=""
-local dbi=luasql.mysql()
-local dbh=assert(dbi:connect(co.DBNAME,co.DBUSER,co.DBPASSWORD))
-local res=dbh:execute([[
-select name,state from ap where state != "UP" and adminstate = "PRODUCTION" and TIMESTAMPDIFF(DAY,lastcontact,NOW())<7
-]])
-row=res:fetch({},"a")
-while row
-do
+for row in sth:rows(true) do
 	exitcode=1 -- just let it warn on accesspoints that are down
 	message=message .. " " .. row.name .. ":" ..row.state
 	status="CRITICAL:"
 	row=res:fetch({},"a")
 end
-
-
-res:close()
+sth:close()
 dbh:close()
-dbi:close()
-io.write(status .. message)
+io.write(status, message,"\n")
 os.exit(exitcode)
