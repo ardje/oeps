aa6ddd0f179e154ddb5459ca33bb3be100c6c63d
Ard van Breemen <ard@telegraafnet.nl>
2014-04-16 14:42:16 +0200

    Renamed

--- backend/management/www/bin/delete-radius-history.lua
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/lua
-require "oeps.db"
-local dbh=assert(oeps.dbhr())
-local sth=assert(dbh:prepare[[
-	DELETE FROM radacct WHERE acctstoptime IS NOT NULL AND TIMESTAMPDIFF(day, acctstoptime,NOW())>7;
-]])
-sth:execute()
-sth:close()
-dbh:close()
